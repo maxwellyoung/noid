357b5ba9835da7a934a79731da90c94ab63192e3
maxwellyoung <maxtheyoung@gmail.com>
2023-04-04 18:32:19 +1200

    create wizard border fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -104,7 +104,7 @@ const Home: NextPage = () => {
   return (
     <>
       <PageLayout>
-        <div className="flex border border-slate-400 p-4">
+        <div className="flex border-b border-t border-slate-400 p-4">
           {!isSignedIn && (
             <div className="flex justify-center">
               <SignInButton />
