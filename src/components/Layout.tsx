d0936b9d978008facca652026e0046527205dd0f
maxwellyoung <maxtheyoung@gmail.com>
2023-04-04 17:59:47 +1200

    logo center

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,17 +3,19 @@ import Image from "next/image";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="flex justify-center">
+    <>
       <Image
         src="/Noid_logo.svg"
         alt="noid logo"
         width={300}
         height={300}
-        className="invisible fixed  left-2 p-4 sm:visible "
+        className="mx-auto flex justify-center  p-4  "
       />
-      <div className="h-full w-full  border-x border-slate-400 md:max-w-2xl">
-        {props.children}
-      </div>
-    </main>
+      <main className="flex justify-center">
+        <div className="h-full w-full  border-x border-slate-400 md:max-w-2xl">
+          {props.children}
+        </div>
+      </main>
+    </>
   );
 };
