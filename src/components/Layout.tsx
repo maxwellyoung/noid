7f5c237fb94bfac318ba6c62fe3e829ca593d740
maxwellyoung <maxtheyoung@gmail.com>
2023-05-14 20:25:38 +1200

    added scrollbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,17 +14,47 @@ export const PageLayout = (props: PropsWithChildren) => {
         />
       </div>
       <main className="flex justify-center">
-        <div className="h-full w-full   border-slate-400 md:max-w-2xl">
+        <div className="h-full w-full border-slate-400 md:max-w-2xl">
           <Image
             src="/Noid_logo.svg"
             alt="noid logo"
             width={300}
             height={300}
-            className="mx-auto flex justify-center p-4  blur-none  "
+            className="mx-auto flex justify-center p-4 blur-none"
           />
           {props.children}
+          <div className="scrolling-footer">
+            <p>
+              <a href="https://www.maxwellyoung.info/code">
+                This is a Twitter clone developed by Maxwell Young using React,
+                Next.js, Tailwind CSS, TRPC, Prisma and Planetscale in the year
+                2023. For more information on this project, click here.
+              </a>
+            </p>
+          </div>
         </div>
       </main>
+      <style jsx>{`
+        .scrolling-footer {
+          // background-color: #111;
+          color: #fff;
+          padding: 8px;
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          animation: scroll-left 20s linear infinite;
+        }
+
+        @keyframes scroll-left {
+          0% {
+            transform: translateX(100%);
+          }
+          100% {
+            transform: translateX(-100%);
+          }
+        }
+      `}</style>
     </>
   );
 };
